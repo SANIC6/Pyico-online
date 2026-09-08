2996f8e370a2f052a78cb66337a849fa391ee724
SANIC6 <somayajivar@gmail.com>
2026-09-08 18:17:36 -0400

    basic-mouse-state

--- app.py
+++ app.py
@@ -4,6 +4,9 @@ import pygame
 
 from tools.constants import COLORS
 
+import logging
+
+logger = logging.getLogger(__name__)
 
 WINDOW_SIZE = (128,128)
 DISPLAY_SCALE = 4
@@ -11,31 +14,40 @@ _ASSETS_DIR = Path(__file__).resolve().parent / "editor/assets"
 
 class Mouse:
     def __init__(self):
-        self.x = 0
-        self.y = 0
+        self.pos =[0,0]
         self.left_button_pressed = False
         self.mouse_idle_img = pygame.image.load(str(_ASSETS_DIR / "mouse" / "mouse_idle.png")).convert_alpha()
-
-    def update(self):
-        self.x, self.y = pygame.mouse.get_pos()
+    
+    def states(self,sprite_editor_rect):
+        if sprite_editor_rect.collidepoint(tuple(self.pos)):
+            print('Sprite-Editor')
+        else:
+            print('not in ')
+    def update(self,sprite_editor_box):
+        self.pos = list(pygame.mouse.get_pos())
         
-        self.x /= DISPLAY_SCALE
-        self.y /= DISPLAY_SCALE
+        self.pos[0] /= DISPLAY_SCALE
+        self.pos[1] /= DISPLAY_SCALE
+
+
+        
+        self.states(sprite_editor_box)
+
         if pygame.mouse.get_pressed()[0]:
             self.left_button_pressed = True
         else:
             self.left_button_pressed = False
-        if self.x < 0:
-            self.x = 0
-        if self.y < 0:
-            self.y = 0
-        if self.x > WINDOW_SIZE[0]:
-            self.x = WINDOW_SIZE[0]
-        if self.y > WINDOW_SIZE[1]:
-            self.y = WINDOW_SIZE[1]
+        if self.pos[0] < 0:
+            self.pos[0] = 0
+        if self.pos[1] < 0:
+            self.pos[1] = 0
+        if self.pos[0] > WINDOW_SIZE[0]:
+            self.pos[0] = WINDOW_SIZE[0]
+        if self.pos[1] > WINDOW_SIZE[1]:
+            self.pos[1] = WINDOW_SIZE[1]
     def draw(self, surface):
         
-        surface.blit(self.mouse_idle_img, (self.x, self.y))
+        surface.blit(self.mouse_idle_img, (self.pos[0], self.pos[1]))
 
 
 class Editor:
@@ -48,14 +60,13 @@ class Editor:
         self.mouse = Mouse()
         self.sprite_editor_box = pygame.Rect(8,16,64,64)
         self.background = pygame.Rect(0,0,WINDOW_SIZE[0],WINDOW_SIZE[1])
-        self.top_bar = pygame.Rect(0, 0, WINDOW_SIZE[0], 10)
-        self.bottom_bar = pygame.Rect(0, WINDOW_SIZE[1]-10, WINDOW_SIZE[0], 10)
-        self.sprites_background = pygame.Rect(0, WINDOW_SIZE[1]-56, WINDOW_SIZE[0], 48)
+        self.top_bar = pygame.Rect(0, 0, WINDOW_SIZE[0], 8)
+        self.bottom_bar = pygame.Rect(0, WINDOW_SIZE[1]-8, WINDOW_SIZE[0], 8)
+        self.sprites_background = pygame.Rect(0, WINDOW_SIZE[1]-40, WINDOW_SIZE[0], 48)
         pygame.display.set_caption("Pyico Editor")
 
     def run(self):
         while self.running:
-            self.mouse.update()
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     self.running = False
@@ -65,8 +76,9 @@ class Editor:
             pygame.draw.rect(self.game_screen, COLORS[5], self.top_bar)
             pygame.draw.rect(self.game_screen, COLORS[0], self.sprites_background)
             pygame.draw.rect(self.game_screen, COLORS[5], self.bottom_bar)
-            pygame.draw.rect(self.game_screen, COLORS[7], self.sprite_editor_box)
+            pygame.draw.rect(self.game_screen, COLORS[0], self.sprite_editor_box)
 
+            self.mouse.update(self.sprite_editor_box)
             self.mouse.draw(self.game_screen)
             self.screen.blit(pygame.transform.scale(self.game_screen, (WINDOW_SIZE[0] * DISPLAY_SCALE, WINDOW_SIZE[1] * DISPLAY_SCALE)), (0, 0))
             pygame.display.flip()
