6a95a41632d12420875788eb182bd7beb4a8c44d
SANIC6 <somayajivar@gmail.com>
2026-09-08 23:32:29 -0400

    Genral-layout-sprite-editor-done

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ import logging
 
 logger = logging.getLogger(__name__)
 
-WINDOW_SIZE = (128,128)
+WINDOW_SIZE = (160,144)
 DISPLAY_SCALE = 4
 _ASSETS_DIR = Path(__file__).resolve().parent / "editor/assets"
 
@@ -16,6 +16,7 @@ class Mouse:
     def __init__(self):
         self.pos =[0,0]
         self.left_button_pressed = False
+        self.color = COLORS[0]
         self.mouse_idle_img = pygame.image.load(str(_ASSETS_DIR / "mouse" / "mouse_idle.png")).convert_alpha()
     
     def states(self,sprite_editor_rect):
@@ -25,7 +26,6 @@ class Mouse:
             print('not in ')
     def update(self,sprite_editor_box):
         self.pos = list(pygame.mouse.get_pos())
-        
         self.pos[0] /= DISPLAY_SCALE
         self.pos[1] /= DISPLAY_SCALE
 
@@ -62,8 +62,16 @@ class Editor:
         self.background = pygame.Rect(0,0,WINDOW_SIZE[0],WINDOW_SIZE[1])
         self.top_bar = pygame.Rect(0, 0, WINDOW_SIZE[0], 8)
         self.bottom_bar = pygame.Rect(0, WINDOW_SIZE[1]-8, WINDOW_SIZE[0], 8)
-        self.sprites_background = pygame.Rect(0, WINDOW_SIZE[1]-40, WINDOW_SIZE[0], 48)
+        self.pallete_rect = pygame.Rect(8-3, 128-40-3, (8*8)+6, 16+6)
+        self.sprites_background = pygame.Rect(128-48, 16, 72, WINDOW_SIZE[1])
         pygame.display.set_caption("Pyico Editor")
+    def draw_pallete(self,x_pos,y_pos):
+        color_index = 0
+        for x in range(8):
+            for y in range(2):
+                if color_index < len(COLORS):
+                    pygame.draw.rect(self.game_screen, COLORS[color_index], pygame.Rect(x_pos+x*8, y_pos+y*8, 8, 8))
+                color_index += 1
 
     def run(self):
         while self.running:
@@ -72,12 +80,14 @@ class Editor:
                     self.running = False
 
             self.game_screen.fill((255, 255, 255))
-            pygame.draw.rect(self.game_screen, COLORS[8], self.background)
+            print(self.mouse.pos[0],self.mouse.pos[1])
+            pygame.draw.rect(self.game_screen, COLORS[1], self.background)
             pygame.draw.rect(self.game_screen, COLORS[5], self.top_bar)
             pygame.draw.rect(self.game_screen, COLORS[0], self.sprites_background)
             pygame.draw.rect(self.game_screen, COLORS[5], self.bottom_bar)
             pygame.draw.rect(self.game_screen, COLORS[0], self.sprite_editor_box)
-
+            pygame.draw.rect(self.game_screen, COLORS[0], self.pallete_rect,3)
+            self.draw_pallete(8, 128-40)
             self.mouse.update(self.sprite_editor_box)
             self.mouse.draw(self.game_screen)
             self.screen.blit(pygame.transform.scale(self.game_screen, (WINDOW_SIZE[0] * DISPLAY_SCALE, WINDOW_SIZE[1] * DISPLAY_SCALE)), (0, 0))
--- tools/constants.py
+++ tools/constants.py
@@ -2,4 +2,4 @@ from pathlib import Path
 
 from tools.color_picker import load_palette
 
-COLORS = load_palette(Path(__file__).resolve().parents[1] / "gd-15-32x.png")
+COLORS = load_palette(Path(__file__).resolve().parents[1] / "16-bital.png")
